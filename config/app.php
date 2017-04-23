5f60f8b1e13211a2f207762114685c32a63c089e
Tom Witkowski <dev.gummibeer@gmail.com>
2017-04-23 22:52:38 +0200

    add log viewer

--- config/app.php
+++ config/app.php
@@ -168,6 +168,7 @@ return [
          */
         Laravel\Tinker\TinkerServiceProvider::class,
         Yajra\Datatables\DatatablesServiceProvider::class,
+        Arcanedev\LogViewer\LogViewerServiceProvider::class,
 
         /*
          * Application Service Providers...
