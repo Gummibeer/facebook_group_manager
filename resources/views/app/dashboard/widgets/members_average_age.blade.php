77d3d9112efcb8d75a104d2f45f91f4a85794755
Tom Witkowski <dev.gummibeer@gmail.com>
2017-04-26 15:16:36 +0200

    add min and max age

--- resources/views/app/dashboard/widgets/members_average_age.blade.php
+++ resources/views/app/dashboard/widgets/members_average_age.blade.php
@@ -4,7 +4,12 @@
             <i class="fa fa-clock-o pull-left"></i>
             <div class="pull-left">
                 <small class="stat-label">{{ trans('labels.members_average_age') }}</small>
-                <h1>{{ round(\App\Models\Member::byActive()->withAge()->avg('age')) }}</h1>
+                <h1>
+                    {{ round(\App\Models\Member::byActive()->withAge()->avg('age')) }}
+                    <small style="color:#ffffff;vertical-align:middle;">
+                    ({{ \App\Models\Member::byActive()->withAge()->min('age') }} - {{ \App\Models\Member::byActive()->withAge()->max('age') }})
+                    </small>
+                </h1>
             </div>
         </div>
     </div>
