86e4e008ac651158e9bf52d0f35c2b816753fa8e
Tom Witkowski <dev.gummibeer@gmail.com>
2017-04-27 14:15:11 +0200

    parse autopost message variables

---
+++ resources/lang/de/date.php
@@ -0,0 +1,14 @@
+<?php
+use Carbon\Carbon;
+
+return [
+    'weekdays' => [
+        Carbon::MONDAY => 'Montag',
+        Carbon::TUESDAY => 'Dienstag',
+        Carbon::WEDNESDAY => 'Mittwoch',
+        Carbon::THURSDAY => 'Donnerstag',
+        Carbon::FRIDAY => 'Freitag',
+        Carbon::SATURDAY => 'Samstag',
+        Carbon::SUNDAY => 'Sonntag',
+    ],
+];
